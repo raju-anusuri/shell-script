4c0b390d12ae891ac7a00c0ecca1686275baedf6
raju-anusuri <rajuanusuri639@gmail.com>
2026-02-14 22:58:05 +0530

    script added

--- 09-install-script.sh
+++ 09-install-script.sh
@@ -8,4 +8,18 @@ echo "ERROR:: you must have sudo access to exicute this script"
 exit 1 #other than 0
 fi
 dnf install mysql -y
+if [ $? -ne 0 ]
+then
+    echo "Installing MySQL... FAILURE"
+    exit 1
+else
+    echo "Installing MySQL... SUCCESS"
+fi
 dnf install git -y
+if [ $? -ne 0 ]
+then
+    echo "Installing GIT... FAILURE"
+    exit 1
+else
+    echo "Installing GIT... SUCCESS"
+fi
